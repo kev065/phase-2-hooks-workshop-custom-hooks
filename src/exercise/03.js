de1b43706500eaaaf58e88070bf8e0b573a2187b
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-04 17:38:36 +0300

    updated

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
+import hasi from "../data/assets/hasi.png";
 
 export function useMouseCoordinates() {
   // ✅ get the setCoordinates function back too!
   // 👀 const [coordinates, setCoordinates] = useState(...)
-  const [coordinates] = useState({
+  const [coordinates, setCoordinates] = useState({
     clientX: 0,
     clientY: 0,
   });
@@ -14,18 +15,23 @@ export function useMouseCoordinates() {
      set state with the clientX and clientY coordinates from the event
      👀 function handler(event) {}
     */
-
+    function handleMouseMove({ clientX, clientY }) {
+      setCoordinates({ clientX, clientY })
+    }
+  
     /* 
      ✅ attach an event listener to the window for the mousemove event
      📃 https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
      👀 window.addEventListener("mousemove", handler)
     */
+    window.addEventListener('mousemove', handleMouseMove)
 
     return function cleanup() {
       /* 
        ✅ make sure to clean up your event listeners when your hook is no longer in use!
        👀 window.removeEventListener("mousemove", handler)
       */
+     window.removeEventListener('mousemove', handleMouseMove)
     };
   }, []);
 
@@ -52,7 +58,7 @@ function Cursor({ x, y }) {
     height: "45px",
     width: "45px",
     borderRadius: "50%",
-    background: "blue",
+    background: `url(${hasi})`,
     backgroundSize: "cover",
     zIndex: 1,
   };
